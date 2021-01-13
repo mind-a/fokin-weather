7feb872fcead18a252ad745fc6af6a89fa12f150
dahyeMin <sol1829@gmail.com>
2021-01-13 22:37:34 +0900

    #0.5 How does React Native Work

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
+      <Text style={styles.text}>Hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
+    //이건CSS처럼 보이는데 리액트 네이티브가 css엔진을 구현했기 때문에 가능한
   },
+  text: {
+    color: 'white',
+  }
 });

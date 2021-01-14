6d08b30f1b23b082c2af3d1b1dec0e0734f9a3c2
dahyeMin <sol1829@gmail.com>
2021-01-14 22:34:07 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
-    //이건CSS처럼 보이는데 리액트 네이티브가 css엔진을 구현했기 때문에 가능한
-  },
-  text: {
-    color: 'white',
-  }
-});
